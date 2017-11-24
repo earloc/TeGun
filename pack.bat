48833e366d5ea0477f541fa199c4e312589344cb
earloc <earloc@live.de>
2017-11-24 08:38:31 +0100

    test for package creation

--- pack.bat
+++ pack.bat
@@ -1,8 +1,12 @@
 @echo off
 
+
+set PacakgeSuffix=-pre
+
 pushd src
 
 echo packing
+echo PacakgeSuffix:'%PacakgeSuffix%'
 echo ------------------
 dotnet pack -c Release --version-suffix "-pre"
 echo packing done
