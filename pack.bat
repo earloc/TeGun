b94e14f341923f4052667992f22b7daf025569ca
earloc <earloc@live.de>
2017-11-28 20:48:42 +0100

    #4 provide template for "dotnet new"

--- pack.bat
+++ pack.bat
@@ -10,5 +10,9 @@ echo PackageSuffix:'%PackageSuffix%'
 echo ------------------
 dotnet pack -c Release --version-suffix "-pre"
 echo packing done
+
+echo packing template
+nuget pack src\dotnet-tegun.template\dotnet-tegun.template.nuspec -OutputDirectory src\dotnet-tegun.template\bin\packages
+echo done packing template
 echo __________________
 popd
